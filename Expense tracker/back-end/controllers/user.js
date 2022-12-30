f088da3b13fed0b030661faaf240561b2101efdc
Hrithik Bagauli <hrithikbagauli@gmail.com>
2022-12-30 15:10:20 +0530

    Hashing passwords before storing

--- Expense tracker/back-end/controllers/user.js
+++ Expense tracker/back-end/controllers/user.js
@@ -1,25 +1,30 @@
 const user = require('../models/user');
+const bcrypt = require('bcrypt');
 
 exports.postUserSignup = (req, res, next) => {
-  user.create({ name: req.body.name, email: req.body.email, password: req.body.password })
-    .then(() => {
-      res.status(200).json({success: true});
-    })
-    .catch(err => {
-      res.status(400).json({success: false});
-    });
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    user.create({ name: req.body.name, email: req.body.email, password: hash })
+      .then(() => {
+        res.status(201).json({ success: true });
+      })
+      .catch(err => {
+        res.status(400).json({ success: false });
+      });
+  })
 }
 
 exports.postUserLogin = (req, res, next) => {
   user.findAll({ where: { email: req.body.email } })
     .then(user => {
       if (user.length > 0) {
-        if (user[0].dataValues.password == req.body.password) {
-          res.status(200).json({ success: true, message: 'Authenticated successfully!' });
-        }
-        else {
-          res.status(400).json({ success: false, message: 'Incorrect password!' });
-        }
+        bcrypt.compare(req.body.password, user[0].dataValues.password, (err, result) => {
+          if (result) {
+            res.status(200).json({ success: true, message: 'Authenticated successfully!' });
+          }
+          else {
+            res.status(400).json({ success: false, message: 'Incorrect password!' });
+          }
+        })
       }
       else {
         res.status(404).json({ success: false, message: "This user doesn't exist!" });
